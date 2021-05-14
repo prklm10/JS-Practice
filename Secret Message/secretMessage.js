7b335c20b428acdbba2fc53c30ec026effc19bce
prklm10 <pradumraj360@gmail.com>
2021-05-15 03:32:23 +0530

    Secret Message

---
+++ Secret Message/secretMessage.js
@@ -0,0 +1,12 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to','Program');
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+//console.log(secretMessage);
+secretMessage.shift();
+//console.log(secretMessage);
+secretMessage.unshift('Programming');
+//console.log(secretMessage);
+secretMessage.splice(secretMessage.indexOf('get'),5,'know');
+console.log(secretMessage.join());
